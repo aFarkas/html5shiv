890383d71980b62857de1110bb4f44fbd2183829
Jonathan Neal <jonathantneal@hotmail.com>
2011-12-15 17:12:58 -0800

    All updates from initial community review pushed to print shiv as well.

--- src/html5shiv-printshiv.js
+++ src/html5shiv-printshiv.js
@@ -1,14 +1,20 @@
 /*! HTML5 Shiv v3 | @jon_neal @afarkas @rem | MIT/GPL2 Licensed */
 (function (win, doc) {
 	// feature detection: whether the browser supports unknown elements
-	var supportsUnknownElements = (function (a) { a.innerHTML = '<x-element></x-element>'; return a.childNodes.length === 1; })(doc.createElement('a'));
+	var supportsUnknownElements = (function (a) {
+		a.innerHTML = '<x-element></x-element>';
+		return a.childNodes.length === 1;
+	})(doc.createElement('a'));
 
 	// feature detection: whether the browser supports default html5 styles
-	var supportsHtml5Styles = (function (nav, docEl, compStyle) { docEl.appendChild(nav); return (compStyle = (compStyle ? compStyle(nav) : nav.currentStyle).display) && docEl.removeChild(nav) && compStyle === 'block'; })(doc.createElement('nav'), doc.documentElement, win.getComputedStyle);
+	var supportsHtml5Styles = (function (nav, docEl, compStyle) {
+		docEl.appendChild(nav);
+		return (compStyle = (compStyle ? compStyle(nav) : nav.currentStyle).display) && docEl.removeChild(nav) && compStyle === 'block';
+	})(doc.createElement('nav'), doc.documentElement, win.getComputedStyle);
 
 	// html5 global so that more elements can be shived and also so that existing shiving can be detected on iframes
 	// more elements can be added and shived with the following code: html5.elements.push('element-name'); shivDocument(document);
-	win.html5 = {
+	var html5 = {
 		// a list of html5 elements
 		elements: 'abbr article aside audio bdi canvas data datalist details figcaption figure footer header hgroup mark meter nav output progress section summary time video'.split(' '),
 
@@ -32,39 +38,43 @@
 			// shiv for unknown elements
 			if (!supportsUnknownElements) {
 				// shiv the document
-				win.html5.elements.join(' ').replace(/\w+/g, documentCreateElementReplaceFunction);
+				html5.elements.join(' ').replace(/\w+/g, documentCreateElementReplaceFunction);
 
 				// shiv document create element function
 				scopeDocument.createElement = function (nodeName) {
 					var element = documentCreateElement(nodeName);
-					if (!/^(input|script)$/i.test(nodeName)) {
-						win.html5.shivDocument(element.document);
-					}
+					if (element.canHaveChildren) html5.shivDocument(element.document);
 					return element;
 				};
 
 				// shiv document create element function
 				scopeDocument.createDocumentFragment = function () {
-					var documentFragment = documentCreateDocumentFragment();
-					win.html5.shivDocument(documentFragment);
-					return documentFragment;
+					return html5.shivDocument(documentCreateDocumentFragment());
 				};
 			}
 
 			// shiv for default html5 styles
 			if (!supportsHtml5Styles && documentHead) {
-				documentHead.insertBefore(documentCreateElement('style'), documentHead.firstChild).styleSheet.cssText = [
+				var div = documentCreateElement('div');
+				div.innerHTML = ['x<style>',
 					'article,aside,details,figcaption,figure,footer,header,hgroup,nav,section{display:block}', // Corrects block display not defined in IE6/7/8/9
-					'canvas,video{display:inline-block;*display:inline;*zoom:1}', // Corrects inline-block display not defined in IE6/7/8/9
-					'[hidden]{display:none}', // Corrects styling for 'hidden' attribute not present in IE7/8/9
-					'mark{background:#FF0;color:#000}' // Addresses styling not present in IE6/7/8/9
-				].join('');
+					'audio{display:none}', // Corrects audio display not defined in IE6/7/8/9
+					'canvas,video{display:inline-block;*display:inline;*zoom:1}', // Corrects canvas and video display not defined in IE6/7/8/9 (audio[controls] in IE7)
+					'[hidden]{display:none}audio[controls]{display:inline-block;*display:inline;*zoom:1}', // Corrects styling for 'hidden' attribute and audio[controls] display not present in IE7/8/9
+					'mark{background:#FF0;color:#000}', // Addresses styling not present in IE6/7/8/9
+				'</style>'].join('');
+				documentHead.insertBefore(div.lastChild, documentHead.firstChild);
 			}
+
+			// return document (for potential chaining)
+			return scopeDocument;
 		}
 	};
 
 	// shiv the document
-	win.html5.shivDocument(doc);
+	html5.shivDocument(doc);
+
+	win.html5 = html5;
 
 	// ie print shiv
 	if (supportsUnknownElements || !win.attachEvent) return;
