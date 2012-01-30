902df7e637f57779ca80d682c86b621fb592807a
aFarkas <info@corrupt-system.de>
2012-01-30 03:12:41 +0100

    changed pickup option

--- src/html5shiv.js
+++ src/html5shiv.js
@@ -20,8 +20,8 @@
 	var html5 = {
 		// a list of html5 elements
 		elements: html5 && html5.elements ? html5.elements : 'abbr article aside audio bdi canvas data datalist details figcaption figure footer header hgroup mark meter nav output progress section summary time video'.split(' '),
-		shivMethods: html5 && html5.shivMethods ? html5.shivMethods : true,
-		shivCSS: html5 && html5.shivCSS ? html5.shivCSS : true,
+		shivMethods: (html5 && html5.shivMethods === false) ? false : true,
+		shivCSS: (html5 && html5.shivCSS === false) ? false : true,
 		shivDocument: function (scopeDocument) {
 			if (supportsUnknownElements || scopeDocument.documentShived) {
 				return;
