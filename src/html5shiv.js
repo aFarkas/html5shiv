ad862c6aa1d5b8173a0deadb81ffb7656d85d0aa
jonathantneal <jonathantneal@hotmail.com>
2012-01-30 09:39:58 -0800

    https://github.com/aFarkas/html5shiv/issues/31 : Potential problem with `documentCreateElement` https://github.com/aFarkas/html5shiv/issues/32 : Firefox 3.x demands second argument for getComputedStyle https://github.com/aFarkas/html5shiv/issues/33 : Elements not being written correctly Non-Issue: Comment cleanup

--- src/html5shiv.js
+++ src/html5shiv.js
@@ -16,7 +16,7 @@
 
 		root.insertBefore(nav, root.firstChild);
 
-		supported = (compStyle ? compStyle(nav) : nav.currentStyle).display === 'block';
+		supported = (compStyle ? compStyle(nav, null) : nav.currentStyle).display === 'block';
 
 		root.removeChild(nav);
 
@@ -24,81 +24,80 @@
 
 		return supported;
 	})(doc.createElement('nav'), doc.documentElement, win.getComputedStyle);
+
+	var html5 = win.html5 || {};
 	
-	var html5 = window.html5 || {};
-	var elements = html5.element || 'abbr article aside audio bdi canvas data datalist details figcaption figure footer header hgroup mark meter nav output progress section summary time video'.split(' ');
+	var defaultHtml5Elements = 'abbr article aside audio bdi canvas data datalist details figcaption figure footer header hgroup mark meter nav output progress section summary time video';
+
+	var call = Date.call;
 
 	// html5 global so that more elements can be shived and also so that existing shiving can be detected on iframes
 	// more elements can be added and shived: html5.elements.push('element-name'); html5.shivDocument(document);
 	// defaults can be changed before the script is included: html5 = { shivMethods: false, shivCSS: false, elements: 'foo bar' };
 	html5 = {
 		// a list of html5 elements
-		elements: elements,
+		elements: (html5.elements || defaultHtml5Elements).split(' '),
 		shivMethods: !(html5.shivMethods === false),
 		shivCSS: !(html5.shivCSS === false),
 		type: 'default',
 		shivDocument: function (scopeDocument) {
-			if (supportsUnknownElements || scopeDocument.documentShived) {
-				return;
-			}
+			if (!supportsUnknownElements && !scopeDocument.documentShived) {
 
-			var
-			documentCreateElement = scopeDocument.createElement,
-			documentCreateDocumentFragment = scopeDocument.createDocumentFragment;
+				var
+				documentCreateElement = scopeDocument.createElement,
+				documentCreateDocumentFragment = scopeDocument.createDocumentFragment;
 
-			// shiv the document
-			for (var i = 0, l = elements.length; i < l; ++i) {
-				documentCreateElement(elements[i]);
-			}
-
-			// shiv document create element function
-			scopeDocument.createElement = function (nodeName) {
-				var element = documentCreateElement(nodeName);
-
-				// don't shiv elements that can't have child nodes or namespaced custom elements to avoid
-				// bugs associated with overwriting `createElement()`
-				if (html5.shivMethods && element.canHaveChildren && !(element.xmlns || element.tagUrn)) {
-					html5.shivDocument(element.document);
+				// shiv the document
+				for (var i = 0, l = html5.elements.length; i < l; ++i) {
+					call.call(documentCreateElement, scopeDocument, html5.elements[i]);
 				}
 
-				return element;
-			};
+				// shiv the document create element method
+				scopeDocument.createElement = function (nodeName) {
+					var element = call.call(documentCreateElement, scopeDocument, nodeName);
 
-			// shiv document create document fragment function
-			scopeDocument.createDocumentFragment = function () {
-				var frag = documentCreateDocumentFragment();
+					// shiv only supported elements (supporting children, not namespaced)
+					if (html5.shivMethods && element.canHaveChildren && !(element.xmlns || element.tagUrn)) {
+						html5.shivDocument(element.document);
+					}
 
-				return (html5.shivMethods) ? html5.shivDocument(frag) : frag;
-			};
+					return element;
+				};
 
-			// set document head as a variable
+				// shiv the document create document fragment method
+				scopeDocument.createDocumentFragment = function () {
+					var frag = call.call(documentCreateDocumentFragment, scopeDocument);
+
+					return (html5.shivMethods) ? html5.shivDocument(frag) : frag;
+				};
+			}
+
+			// set the document head variable
 			var documentHead = scopeDocument.getElementsByTagName('head')[0];
 
-			// shiv for default html5 styles
+			// shiv the default html5 styles
 			if (html5.shivCSS && !supportsHtml5Styles && documentHead) {
-				var div = scopeDocument.createElement('div');
+				var x = scopeDocument.createElement('x-element');
 
-				div.innerHTML = 'x<style>' +
+				x.innerHTML = 'x<style>' +
 					'article,aside,details,figcaption,figure,footer,header,hgroup,nav,section{display:block}' + // Corrects block display not defined in IE6/7/8/9
 					'audio{display:none}' + // Corrects audio display not defined in IE6/7/8/9
-					'canvas,video{display:inline-block;*display:inline;*zoom:1}' + // Corrects canvas and video display not defined in IE6/7/8/9 (audio[controls] in IE7)
+					'canvas,video{display:inline-block;*display:inline;*zoom:1}' + // Corrects canvas and video display not defined in IE6/7/8/9
 					'[hidden]{display:none}audio[controls]{display:inline-block;*display:inline;*zoom:1}' + // Corrects 'hidden' attribute and audio[controls] display not present in IE7/8/9
 					'mark{background:#FF0;color:#000}' + // Addresses styling not present in IE6/7/8/9
 				'</style>';
 
-				documentHead.insertBefore(div.lastChild, documentHead.firstChild);
+				documentHead.insertBefore(x.lastChild, documentHead.firstChild);
 			}
 
-			// set document as shivved
+			// set the document as shivved
 			scopeDocument.documentShived = true;
 
-			// return document
+			// return the document
 			return scopeDocument;
 		}
 	};
 
 	// shiv the document
 	html5.shivDocument(doc);
-
-	win.html5 = html5;
 })(this, document);
