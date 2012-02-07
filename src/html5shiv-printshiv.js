45407517c90d5244e4a1bb044164f61b44045a58
aFarkas <info@corrupt-system.de>
2012-02-07 08:33:31 +0100

    3.3 final

--- src/html5shiv-printshiv.js
+++ src/html5shiv-printshiv.js
@@ -1,4 +1,4 @@
-/*! HTML5 Shiv v3.3RC1 | @afarkas @jdalton @jon_neal @rem | MIT/GPL2 Licensed */
+/*! HTML5 Shiv v3.3 | @afarkas @jdalton @jon_neal @rem | MIT/GPL2 Licensed */
 ;(function(window, document) {
 
   /** Preset options */
