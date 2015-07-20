e5cb874fcb785f8fd1233d15dd97b8fff4dccb7a
aFarkas <info@corrupt-system.de>
2015-07-20 22:03:13 +0200

    3.7.3

--- src/html5shiv.js
+++ src/html5shiv.js
@@ -1,10 +1,10 @@
 /**
-* @preserve HTML5 Shiv 3.7.3-pre | @afarkas @jdalton @jon_neal @rem | MIT/GPL2 Licensed
+* @preserve HTML5 Shiv 3.7.3 | @afarkas @jdalton @jon_neal @rem | MIT/GPL2 Licensed
 */
 ;(function(window, document) {
 /*jshint evil:true */
   /** version */
-  var version = '3.7.3-pre';
+  var version = '3.7.3';
 
   /** Preset options */
   var options = window.html5 || {};
